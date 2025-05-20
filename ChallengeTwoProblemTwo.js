f22d6464b4341ac523e38348b58d7561a4cddb0b
Miyahara <25MiyaharaB@badinhs.org>
2025-05-20 09:55:07 -0400

    finnished but I have issues and I can't get it to work

--- ChallengeTwoProblemTwo.js
+++ ChallengeTwoProblemTwo.js
@@ -1,1 +1,32 @@
-//hi
+// the gcd function gives you the greatest common divisor
+
+function gcd (a,b){
+    while (b !== 0) {
+       
+       
+        [a,b] = [b, a % b]
+    }
+    return a;
+
+
+
+//function lcm gives you the least common multiple i believe
+
+function lcm (a, b) {
+    return (a * b) / gcd (a, b);
+}
+
+
+function smallestMultiple (limit){
+
+
+    let result =1}
+
+for (let i = 2; i <= limit; i++) {
+
+}
+return result;
+}
+
+
+console.log(smallestMultiple(20));
